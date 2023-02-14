cde1b734d047edf7f61c36a60d3a0709d5f068c6
unknown <ronaldofernandus10.com>
2023-02-14 14:44:21 +0700

    home

--- food/src/components/Banner.vue
+++ food/src/components/Banner.vue
@@ -1,17 +1,44 @@
 <template>
-  <div class="row mt-4">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2><strong>Food Menu</strong><br />in Your Gadget</h2>
-          <p>silahkan pilih makanan anda</p>
-          <button class="btn btn-lg btn-success"><b-icon-arrow-right/>Pesan Sekarang</button>
+  <div class="banner">
+    <!--Desktop-->
+    <div class="d-none d-md-block">
+      <div class="row mt-4">
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Food Menu</strong><br />in Your Gadget</h2>
+              <p>silahkan pilih makanan anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right />Pesan Sekarang
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-6">
+          <img src="../assets/images/1.png" width="50%" />
         </div>
       </div>
     </div>
 
-    <div class="col">
-      <img src="../assets/images/1.png" width="50%" />
+    <!--Mobile-->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-4">
+        <div class="col-md-6 mb-3">
+          <img src="../assets/images/1.png" width="50%" />
+        </div>
+        <div class="col-md-6">
+          <div class="d-flex h-100">
+            <div class="justify-content-center align-self-center">
+              <h2><strong>Food Menu</strong><br />in Your Gadget</h2>
+              <p>silahkan pilih makanan anda</p>
+              <button class="btn btn-lg btn-success">
+                <b-icon-arrow-right />Pesan Sekarang
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
